513ff61d8df6c51ca1d15fb97ef63adbf135a125
A7ocin <mr_nico@hotmail.it>
2015-07-23 14:18:58 +0200

    Ripristino nomi variabili

--- amm2015/Progetto/php/view/administrator/el_modelli_json.php
+++ amm2015/Progetto/php/view/administrator/el_modelli_json.php
@@ -2,17 +2,17 @@
 
 $json = array();
 $json['errori'] = $errori;
-$json['modelli'] = array();
-foreach($modelli as $modello){ echo " (el_modelli_json foreach) ";
-     /* @var $modello Model */
+$json['models'] = array();
+foreach($models as $model){ echo " (el_modelli_json foreach) ";
+     /* @var $model Model */
     $element = array();
-    $element['id'] = $modello->getId();
-    $element['data'] = $modello->getData()->format('Y-m-d');
-    $element['dimensione'] = $modello->getDimensione();
-    $element['nome'] = $modello->getNome();
-    $element['uploader'] = $modello->getUploader();
-    $element['descrizione'] = $modello->getDescrizione();
-    $json['modelli'][] = $element;
+    $element['id'] = $model->getId();
+    $element['data'] = $model->getData()->format('Y-m-d');
+    $element['dimensione'] = $model->getDimensione();
+    $element['nome'] = $model->getNome();
+    $element['uploader'] = $model->getUploader();
+    $element['descrizione'] = $model->getDescrizione();
+    $json['models'][] = $element;
     
 }
 echo json_encode($json);
