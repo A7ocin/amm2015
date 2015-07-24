248da0b262b7b8d3a051d02f0783d86ff1d85599
A7ocin <mr_nico@hotmail.it>
2015-07-24 22:11:32 +0200

    Inizio pulizia: index.php ripulita e tradotta in inglese

--- amm2015/Progetto/php/index.php
+++ amm2015/Progetto/php/index.php
@@ -1,68 +1,33 @@
 <?php
 
-
 include_once 'controller/BaseController.php';
-include_once 'controller/StudenteController.php';
-include_once 'controller/DocenteController.php';
 include_once 'controller/AdministratorController.php';
 include_once 'controller/ArtistController.php';
 include_once 'controller/UserController.php';
 
 date_default_timezone_set("Europe/Rome");
-// punto unico di accesso all'applicazione
 FrontController::dispatch($_REQUEST);
 
 /**
- * Classe che controlla il punto unico di accesso all'applicazione
- * @author Davide Spano
+ * This is the start page
+ * @author Nicola Garau
  */
 class FrontController {
 
     /**
-     * Gestore delle richieste al punto unico di accesso all'applicazione
-     * @param array $request i parametri della richiesta
+     * This is the request manager
+     * @param array $request the request parameters
      */
     public static function dispatch(&$request) {
-        // inizializziamo la sessione 
+        // let's start the session
         session_start();
         if (isset($request["page"])) {
 
             switch ($request["page"]) {
                 case "login":
-                    // la pagina di login e' accessibile a tutti,
-                    // la facciamo gestire al BaseController
+                    // Since everyone can access the login page,
+                    // it should be controlled by the BaseController
                     $controller = new BaseController();
-                    $controller->handleInput($request);
-                    break;
-                
-                case "info":
-                    $controller = new BaseController();
-                    $controller->handleInput($request);
-                    break;
-
-                // studente
-                case 'studente':
-                    // la pagina degli studenti e' accessibile solo agli studenti
-                    // agli studenti ed agli amminstratori
-                    // il controllo viene fatto dal controller apposito
-                    $controller = new StudenteController();
-                    if (isset($_SESSION[BaseController::role]) &&
-                        $_SESSION[BaseController::role] != User::Studente) {
-                        self::write403();
-                    }
-                    $controller->handleInput($request);
-                    break;
-
-                // docente
-                case 'docente':
-                    // la pagina dei docenti e' accessibile solo
-                    // ai docenti ed agli amminstratori
-                    // il controllo viene fatto dal controller apposito
-                    $controller = new DocenteController();
-                    if (isset($_SESSION[BaseController::role]) &&
-                        $_SESSION[BaseController::role] != User::Docente)  {
-                        self::write403();
-                    }
                     $controller->handleInput($request);
                     break;
                     
@@ -106,10 +71,10 @@ class FrontController {
     }
 
     /**
-     * Crea una pagina di errore quando il path specificato non esiste
+     * Create an error page if the path doesn't exist
      */
     public static function write404() {
-        // impostiamo il codice della risposta http a 404 (file not found)
+        // set http response code to 404 (file not found)
         header('HTTP/1.0 404 Not Found');
         $titolo = "File not found!";
         $messaggio = "The page you requested is not available";
@@ -118,11 +83,10 @@ class FrontController {
     }
 
     /**
-     * Crea una pagina di errore quando l'utente non ha i privilegi 
-     * per accedere alla pagina
+     * Create an error page if the user is not allowed to access the page
      */
     public static function write403() {
-        // impostiamo il codice della risposta http a 404 (file not found)
+        // set http response code to 403 (forbidden)
         header('HTTP/1.0 403 Forbidden');
         $titolo = "Access denied";
         $messaggio = "You don't have the rights to access this page";
